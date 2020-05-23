210a667b9a73d0a9162f4103a6168c70859e89f5
gqc <2017212855@bupt.edu.cn>
2020-05-23 17:45:22 +0800

    add message_struct

--- message_struct.h
+++ message_struct.h
@@ -2,15 +2,27 @@
 #define _MESSAGE_STRUCT
 
 struct DNS_RR{
-
+    unsigned char *name;
+    unsigned short type;
+    unsigned short _class;
+    unsigned int ttl;
+    unsigned short data_len;
+    unsigned char *rdata;
 };
 
 struct DNS_Header{
-
+    unsigned short id;
+    unsigned short tag;     // 包含QR到Rcode的定义
+    unsigned short queryNum;
+    unsigned short answerNum;
+    unsigned short authorNum;
+    unsigned short addNum;
 };
 
 struct DNS_Query{
-
+    unsigned char *name;
+    unsigned short qtype;
+    unsigned short qclass;
 };
 
 
